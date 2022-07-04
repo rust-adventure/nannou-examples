abe67c723fb31794da660fa78ed6f3f4736bbeb6
Christopher Biscardi <chris@christopherbiscardi.com>
2022-07-04 15:06:23 -0700

    base perlin noise

--- sketches/perlin-noise-1d-graph/src/main.rs
+++ sketches/perlin-noise-1d-graph/src/main.rs
@@ -1,22 +1,28 @@
-use std::time::Instant;
+use names::Generator;
+use nannou::{
+    glam::Vec2,
+    noise::{BasicMulti, NoiseFn, Perlin, Seedable},
+    prelude::*,
+};
 
-use nannou::glam::Vec2;
-use nannou::noise::NoiseFn;
-use nannou::{noise::Perlin, prelude::*};
+// interesting variables
+const NOISE_STEP: f32 = 500.;
+const NOISE_SCALE: f32 = 1.;
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 struct Model {
-    noise: Perlin,
+    noise: BasicMulti,
     points: Vec<Vec2>,
+    frame_start: u64,
 }
 
 fn model(app: &App) -> Model {
     let _window = app
         .new_window()
-        .size(1024, 256)
+        .size(1200, 600)
         .view(view)
         .key_pressed(key_pressed)
         .mouse_pressed(mouse_pressed)
@@ -24,16 +30,22 @@ fn model(app: &App) -> Model {
         .unwrap();
 
     Model {
-        noise: Perlin::new(),
+        noise: BasicMulti::new(),
         points: vec![],
+        frame_start: 0,
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
     let win_rect = app.window_rect();
 
-    let tick = app.time;
-    let y = model.noise.get([tick.into(), 0.]);
+    // advance by 1/500 per frame
+    let current_step = (app.elapsed_frames()
+        - model.frame_start) as f32
+        / NOISE_STEP;
+    let y = model
+        .noise
+        .get([(current_step * NOISE_SCALE).into(), 0.]);
     let mapped_y = map_range(
         y,
         -1.0,
@@ -41,19 +53,27 @@ fn update(app: &App, model: &mut Model, _update: Update) {
         win_rect.top(),
         win_rect.bottom(),
     );
-    model.points.push(pt2(tick, mapped_y));
+    model.points.push(pt2(current_step, mapped_y));
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    let background = rgb(0.439, 0.039, 0.467);
+    let foreground = rgb(0.855, 0.31, 0.671);
+
+    // set up containing rectangles
     let win_rect = app.window_rect();
-    // get canvas to draw on
-    let draw = app.draw();
-
-    // set background to blue
-    draw.background().color(rgb(0.439, 0.039, 0.467));
-
     let win_p = win_rect.pad(25.0);
 
+    let draw = app.draw();
+    draw.background().color(background);
+
+    // x=0 line
+    draw.line()
+        .start(Vec2::new(win_rect.left(), 0.))
+        .end(Vec2::new(win_rect.right(), 0.))
+        .color(rgb(0.255, 0.02, 0.275));
+
+    // noise line
     draw.polyline()
         .x(0 as f32 - model.points.len() as f32)
         .weight(1.0)
@@ -61,40 +81,63 @@ fn view(app: &App, model: &Model, frame: Frame) {
             model
                 .points
                 .iter()
+                .cloned()
                 .enumerate()
-                .map(|(i, v)| {
-                    let mut v2 = v.clone();
-                    v2.x = i as f32;
-                    v2
+                .map(|(index, mut point)| {
+                    point.x = index as f32;
+                    point
                 })
                 .collect::<Vec<Vec2>>(),
         )
-        .color(rgb(0.855, 0.31, 0.671));
+        .color(foreground);
 
+    // current noise dot as ellipse
     draw.ellipse()
         .x(0.)
         .y(model.points.iter().last().unwrap().y)
-        .w_h(50.0, 50.0)
-        .color(rgb(0.855, 0.31, 0.671));
+        .w_h(10.0, 10.0)
+        .color(foreground);
 
-    draw.text(&format!(
-        "x: {}\ny: {}",
-        app.time,
-        model.points.iter().last().unwrap().y
-    ))
-    .font_size(24)
-    .wh(win_rect.wh())
-    .left_justify()
-    .align_text_bottom()
-    .color(rgb(0.855, 0.31, 0.671));
+    for i in 0..(app.elapsed_frames() / 500) {
+        draw.line()
+            .start(Vec2::new(
+                (i * 500 - model.points.len() as u64)
+                    as f32,
+                win_rect.top(),
+            ))
+            .end(Vec2::new(
+                (i * 500 - model.points.len() as u64)
+                    as f32,
+                win_rect.bottom(),
+            ));
+    }
+    // x = noise input
+    // y = value
+    // draw.text(&format!(
+    //     "x: {}\ny: {}",
+    //     app.elapsed_frames() as f32 / 500.0,
+    //     model.points.iter().last().unwrap().y
+    // ))
+    // .font_size(24)
+    // .wh(win_rect.wh())
+    // .left_justify()
+    // .align_text_bottom()
+    // .color(foreground);
 
-    draw.text(&format!("FPS: {}", app.fps().floor(),))
+    // display noise seed
+    let seed = model.noise.seed();
+    let seed_bytes = seed.to_be_bytes();
+    let seed_display =
+        std::str::from_utf8(&seed_bytes).unwrap();
+
+    draw.text(&format!("{}", seed_display))
         .font_size(48)
-        .wh(win_rect.wh())
+        .wh(win_p.wh())
         .right_justify()
         .align_text_bottom()
-        .color(rgb(0.855, 0.31, 0.671));
+        .color(foreground);
 
+    // draw to frame
     draw.to_frame(app, &frame).unwrap();
 }
 
@@ -112,9 +155,30 @@ fn key_pressed(app: &App, _model: &mut Model, key: Key) {
 }
 
 fn mouse_pressed(
-    _app: &App,
+    app: &App,
     model: &mut Model,
     _button: MouseButton,
 ) {
-    model.noise = Perlin::new();
+    // generate a new seed using ascii characters, which
+    // tend to be more human-readable than numbers
+    let mut generator = Generator::default();
+    let seed = generator.next().unwrap();
+    let mut bytes: [u8; 4] = [0; 4];
+    for (i, byte) in
+        seed.as_bytes().iter().take(4).enumerate()
+    {
+        bytes[i] = *byte;
+    }
+    let seed_number: u32 = u32::from_be_bytes(bytes);
+
+    // instantiate the new noise
+    let noise = BasicMulti::new();
+    let seeded_noise = noise.set_seed(seed_number);
+
+    // set relevant values on model,
+    // clearing any old data so that data
+    // from previous seeds doesn't render
+    model.noise = seeded_noise;
+    model.frame_start = app.elapsed_frames();
+    model.points = vec![];
 }
